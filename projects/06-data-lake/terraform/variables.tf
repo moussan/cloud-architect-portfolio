5bc1207bcf0ab761cf3ee57afcb19458ae406b72
Moussa El Najmi <moussan@gmail.com>
2025-11-20 14:39:38 -0700

    Add Terraform variables for project configuration

--- projects/06-data-lake/terraform/variables.tf
+++ projects/06-data-lake/terraform/variables.tf
@@ -1,1 +1,14 @@
+variable "project_name" {
+  type    = string
+  default = "data-lake"
+}
 
+variable "region" {
+  type    = string
+  default = "ca-central-1"
+}
+
+variable "glue_role_arn" {
+  type        = string
+  description = "IAM role ARN for Glue crawler"
+}
