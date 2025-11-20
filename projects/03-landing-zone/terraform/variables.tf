4697290e159a12847fdc8e33e3f2fc221a164d83
Moussa El Najmi <moussan@gmail.com>
2025-11-20 14:34:02 -0700

    Add variables for region and account emails

--- projects/03-landing-zone/terraform/variables.tf
+++ projects/03-landing-zone/terraform/variables.tf
@@ -1,1 +1,19 @@
+variable "region" {
+  type        = string
+  default     = "us-east-1"
+}
 
+variable "security_account_email" {
+  type        = string
+  description = "Email for the security account"
+}
+
+variable "prod_account_email" {
+  type        = string
+  description = "Email for the prod account"
+}
+
+variable "allowed_regions" {
+  type        = list(string)
+  default     = ["ca-central-1", "us-east-1"]
+}
