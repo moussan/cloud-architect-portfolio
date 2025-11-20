4c952350060c5de09c6f3f059ebbd96d313cb2cf
Moussa El Najmi <moussan@gmail.com>
2025-11-20 14:30:45 -0700

    Add outputs for S3 bucket, DynamoDB table, and SNS topic

--- projects/02-serverless-pipeline/terraform/outputs.tf
+++ projects/02-serverless-pipeline/terraform/outputs.tf
@@ -1,1 +1,11 @@
+output "ingest_bucket" {
+  value = aws_s3_bucket.ingest.bucket
+}
 
+output "dynamodb_table" {
+  value = aws_dynamodb_table.events.name
+}
+
+output "sns_topic_arn" {
+  value = aws_sns_topic.notifications.arn
+}
