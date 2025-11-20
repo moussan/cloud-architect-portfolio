eb3d81b3cec8121be505bd4cf3091a7b54e4b8ae
Moussa El Najmi <moussan@gmail.com>
2025-11-20 14:34:47 -0700

    Add EKS deployment and service configuration

--- projects/04-eks-application-deployment/terraform/main.tf
+++ projects/04-eks-application-deployment/terraform/main.tf
@@ -1,1 +1,95 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
+provider "aws" {
+  region = var.region
+}
+
+data "aws_eks_cluster" "this" {
+  name = var.cluster_name
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = var.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
+resource "kubernetes_namespace" "app" {
+  metadata {
+    name = var.namespace
+  }
+}
+
+resource "kubernetes_deployment" "web" {
+  metadata {
+    name      = "sample-web"
+    namespace = kubernetes_namespace.app.metadata[0].name
+    labels = {
+      app = "sample-web"
+    }
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        app = "sample-web"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "sample-web"
+        }
+      }
+
+      spec {
+        container {
+          name  = "web"
+          image = var.image
+
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "web" {
+  metadata {
+    name      = "sample-web"
+    namespace = kubernetes_namespace.app.metadata[0].name
+  }
+
+  spec {
+    selector = {
+      app = "sample-web"
+    }
+
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
+}
