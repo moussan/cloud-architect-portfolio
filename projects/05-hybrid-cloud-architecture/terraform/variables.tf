8d8c48f8a59302b51e52cdaefe47d1f1a2f394dc
Moussa El Najmi <moussan@gmail.com>
2025-11-20 14:38:59 -0700

    Add variables for hybrid cloud architecture

--- projects/05-hybrid-cloud-architecture/terraform/variables.tf
+++ projects/05-hybrid-cloud-architecture/terraform/variables.tf
@@ -1,1 +1,31 @@
+variable "project_name" {
+  type    = string
+  default = "hybrid-cloud"
+}
 
+variable "region" {
+  type    = string
+  default = "ca-central-1"
+}
+
+variable "prod_vpc_id" {
+  type = string
+}
+
+variable "shared_vpc_id" {
+  type = string
+}
+
+variable "prod_tgw_subnet_ids" {
+  type = list(string)
+}
+
+variable "shared_tgw_subnet_ids" {
+  type = list(string)
+}
+
+variable "allowed_prefixes" {
+  type        = list(string)
+  description = "On-prem prefixes allowed via DXGW"
+  default     = ["10.0.0.0/8"]
+}
