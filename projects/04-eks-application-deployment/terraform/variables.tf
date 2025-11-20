cf2116a7e1659bfbf1a6b87dc2a2e3b5be2163ed
Moussa El Najmi <moussan@gmail.com>
2025-11-20 14:35:06 -0700

    Update variables.tf

--- projects/04-eks-application-deployment/terraform/variables.tf
+++ projects/04-eks-application-deployment/terraform/variables.tf
@@ -1,1 +1,20 @@
+variable "region" {
+  type    = string
+  default = "ca-central-1"
+}
 
+variable "cluster_name" {
+  type        = string
+  description = "Existing EKS cluster name"
+}
+
+variable "namespace" {
+  type    = string
+  default = "sample-app"
+}
+
+variable "image" {
+  type        = string
+  default     = "nginx:stable"
+  description = "Container image for the sample web app"
+}
