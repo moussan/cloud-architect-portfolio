d1018c6798f4707bf7cb770fbc79d4c6ebdb0067
Moussa El Najmi <moussan@gmail.com>
2025-11-20 14:30:31 -0700

    Add variables for project configuration

--- projects/02-serverless-pipeline/terraform/variables.tf
+++ projects/02-serverless-pipeline/terraform/variables.tf
@@ -1,1 +1,14 @@
+variable "project_name" {
+  type        = string
+  default     = "serverless-pipeline"
+}
 
+variable "region" {
+  type        = string
+  default     = "ca-central-1"
+}
+
+variable "env" {
+  type        = string
+  default     = "dev"
+}
